90afe622262cbfce8568195cee2597247ea9be44
HyScript7 <hyscript7@seznam.cz>
2025-11-26 22:36:38 +0100

    Initial Commit
    The initial commit for Ascendency.
    
    Setup for development with Paper 1.21.8 using Java 21 and a
    bundled datapack for 1.21.8 - 1.21.9.
    
    Includes resources for logo and banner.

---
+++ src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
@@ -0,0 +1,16 @@
+package io.github.hyscript7.ascendancy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AscendancyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
