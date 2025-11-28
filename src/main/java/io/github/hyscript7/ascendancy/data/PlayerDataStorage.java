e67d59fbd51f84044630f275c2850fb87fb9fd85
HyScript7 <hyscript7@seznam.cz>
2025-11-28 09:07:18 +0100

    Implement a player data handler with JSON
    This is essentially a 1:1 copy from the prototype repository.
    
    Player Data instances are managed by the Player Data Manager (obviously).
    The instantiation of new data instances and retrieval of instances should be done through it.
    In actuality, it delegates load requests to the PlayerDataStorage passed to it in the constructor.
    
    Also implements custom exceptions for singleton instantiation and retrieval.

---
+++ src/main/java/io/github/hyscript7/ascendancy/data/PlayerDataStorage.java
@@ -0,0 +1,14 @@
+package io.github.hyscript7.ascendancy.data;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public interface PlayerDataStorage {
+    PlayerData load(UUID uuid) throws IOException;
+
+    void save(PlayerData data) throws IOException;
+
+    boolean exists(UUID uuid);
+
+    void delete(UUID uuid) throws IOException;
+}
