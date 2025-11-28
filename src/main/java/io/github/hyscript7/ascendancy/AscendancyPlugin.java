d9461af2a579fc7f991762465577472b1e780604
HyScript7 <hyscript7@seznam.cz>
2025-11-28 20:35:06 +0100

    Register the listeners for innate commands and player data

--- src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
+++ src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
@@ -1,9 +1,13 @@
 package io.github.hyscript7.ascendancy;
 
+import io.github.hyscript7.ascendancy.data.players.PlayerDataListener;
 import io.github.hyscript7.ascendancy.data.players.storage.JsonPlayerDataStorage;
 import io.github.hyscript7.ascendancy.data.players.PlayerDataManager;
 import io.github.hyscript7.ascendancy.data.players.storage.PlayerDataStorage;
 import io.github.hyscript7.ascendancy.data.players.names.TrueNameManager;
+import io.github.hyscript7.ascendancy.features.innate.names.listeners.InnateCommandListener;
+import io.github.hyscript7.ascendancy.registries.RegistryManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AscendancyPlugin extends JavaPlugin {
@@ -28,6 +32,12 @@ public final class AscendancyPlugin extends JavaPlugin {
         );
         getLogger().info("True Names indexed successfully");
 
+        RegistryManager.getInstance().initialize();
+        getLogger().info("Registries initialized successfully");
+
+        registerListeners();
+        getLogger().info("Listeners registered successfully");
+
         getLogger().info("Plugin enabled successfully!");
     }
 
@@ -39,6 +49,11 @@ public final class AscendancyPlugin extends JavaPlugin {
             getLogger().warning("PlayerDataManager hasn't been initialized yet!");
         }
         getLogger().info("Plugin disabled successfully!");
+    }
+
+    private void registerListeners() {
+        Bukkit.getServer().getPluginManager().registerEvents(new PlayerDataListener(), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new InnateCommandListener(), this);
     }
 
     public static AscendancyPlugin getInstance() {
