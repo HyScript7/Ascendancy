5ccc368101cb9b440efee1f7ba5702b7ba68d7c7
HyScript7 <hyscript7@seznam.cz>
2025-11-29 00:53:30 +0100

    Centralize loading the config for quick access

---
+++ src/main/java/io/github/hyscript7/ascendancy/AscendancyConfig.java
@@ -0,0 +1,61 @@
+package io.github.hyscript7.ascendancy;
+
+import lombok.Getter;
+import org.bukkit.configuration.file.FileConfiguration;
+
+@Getter
+public class AscendancyConfig {
+    private static AscendancyConfig instance;
+    public static AscendancyConfig getInstance() {
+        if (instance == null) {
+            instance = new AscendancyConfig(AscendancyPlugin.getInstance());
+        }
+        return instance;
+    }
+
+    public record InnateNames(Honeypot honeypot) {
+        public record Honeypot(String name, boolean enabled) {}
+    }
+
+    public record VoidBan(int defaultMaxLives, int startingLives, int resurrectionLives, int reviveLives, boolean pveEnabled) {}
+
+    public record VoidRealm(int escapeHeightOvershootPercentage, Strain strain, DeadPlayerRestrictions deadPlayerRestrictions) {
+        public record Strain(boolean enabled, boolean targetDead, boolean targetLiving, boolean targetNonPlayers) {}
+        public record DeadPlayerRestrictions(boolean noBuild, boolean noBreak, boolean noInteract) {}
+    }
+
+    private final InnateNames innateNames;
+    private final VoidBan voidBan;
+    private final VoidRealm voidRealm;
+
+    private AscendancyConfig(AscendancyPlugin plugin) {
+        FileConfiguration config = plugin.getConfig();
+        this.innateNames = new InnateNames(
+                new InnateNames.Honeypot(
+                        config.getString("innate_names.honeypot.name", "Obscuron"),
+                        config.getBoolean("innate_names.honeypot.enabled", false)
+                )
+        );
+        this.voidBan = new VoidBan(
+                config.getInt("void_ban.default_max_lives", 10),
+                config.getInt("void_ban.starting_lives", 10),
+                config.getInt("void_ban.resurrection_lives", 10),
+                config.getInt("void_ban.revive_lives", 10),
+                config.getBoolean("void_ban.pve_enabled", false)
+        );
+        this.voidRealm = new VoidRealm(
+                config.getInt("void_realm.escape_height_overshoot_percentage", 10),
+                new VoidRealm.Strain(
+                        config.getBoolean("void_realm.strain.enabled", true),
+                        config.getBoolean("void_realm.strain.target_dead", true),
+                        config.getBoolean("void_realm.strain.target_living", true),
+                        config.getBoolean("void_realm.strain.target_non_players", false)
+                ),
+                new VoidRealm.DeadPlayerRestrictions(
+                        config.getBoolean("void_realm.dead_player_restrictions.no_build", true),
+                        config.getBoolean("void_realm.dead_player_restrictions.no_break", true),
+                        config.getBoolean("void_realm.dead_player_restrictions.no_interact", false)
+                )
+        );
+    }
+}
