b853cb8ca8b4da2b20b1152ffa50469dc90f31cc
HyScript7 <hyscript7@seznam.cz>
2025-12-03 13:44:31 +0100

    Fix bestow code being terrible

--- src/main/java/io/github/hyscript7/ascendancy/builtins/innate/commands/other/BestowItem.java
+++ src/main/java/io/github/hyscript7/ascendancy/builtins/innate/commands/other/BestowItem.java
@@ -4,9 +4,10 @@ import io.github.hyscript7.ascendancy.AscendancyMessagingAPI;
 import io.github.hyscript7.ascendancy.features.innate.names.AbstractInnateCommand;
 import io.github.hyscript7.ascendancy.features.innate.names.InnateContext;
 import io.github.hyscript7.ascendancy.features.innate.names.InnateUtils;
-import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,30 +23,43 @@ public class BestowItem extends AbstractInnateCommand {
         int count = Integer.parseInt(context.args()[0]);
         String thing = context.args()[1];
 
-        Material material = Material.getMaterial(thing.replace(" ", "_").toUpperCase());
-        if (material == null || context.invoker().getInventory().contains(material)) {
-            ItemStack theThing = null;
-            int actualCount = 0;
-            for (ItemStack item : context.invoker().getInventory().getContents()) {
-                if (item != null) {
-                    actualCount = Math.min(item.getAmount(), count);
-                    theThing = item.clone();
-                    theThing.setAmount(actualCount);
-                    context.invoker().getInventory().remove(theThing);
-                    break;
-                }
-            }
-            if (theThing != null) {
-                context.target().getInventory().addItem(theThing);
-                String itemAndCount = actualCount + " " + thing.toLowerCase();
-                AscendancyMessagingAPI.getInstance().send(context.target(), AscendancyMessagingAPI.MessageType.INFO, "You have been bestowed" + itemAndCount +" by " + context.invoker().getName() + "!");
-                AscendancyMessagingAPI.getInstance().send(context.invoker(), AscendancyMessagingAPI.MessageType.SUCCESS, "You have bestowed " + context.target().getName() + itemAndCount + "!");
-            } else {
-                AscendancyMessagingAPI.getInstance().send(context.invoker(), AscendancyMessagingAPI.MessageType.ERROR, "You do not have any " + thing.toLowerCase() + "!");
-                return false;
+        Material material = Material.matchMaterial(thing);
+
+        ItemStack theThing = removeOnceFromInvokerAndReturnItemStack(context.invoker(), material, count);
+        if (theThing != null) {
+            count = theThing.getAmount();
+            addToTarget(context.target(), theThing);
+        }
+
+        if (material != null && theThing != null) {
+            AscendancyMessagingAPI.getInstance().send(context.target(), AscendancyMessagingAPI.MessageType.INFO, "You have been bestowed " + count +" by " + context.invoker().getName() + "!");
+            AscendancyMessagingAPI.getInstance().send(context.invoker(), AscendancyMessagingAPI.MessageType.SUCCESS, "You have bestowed " + context.target().getName() + " " +  count + "x " + thing.toLowerCase() + "!");
+            return true;
+        } else {
+            AscendancyMessagingAPI.getInstance().send(context.invoker(), AscendancyMessagingAPI.MessageType.ERROR, "You do not have any " + thing.toLowerCase() + " that you could bestow!");
+            return false;
+        }
+    }
+
+    private @Nullable ItemStack removeOnceFromInvokerAndReturnItemStack(Player player, Material material, int userAmount) {
+        ItemStack firstMatch = null;
+        for (ItemStack itemStack : player.getInventory().getContents()) {
+            if (itemStack != null && itemStack.getType() == material) {
+                firstMatch = itemStack.clone();
+                int amount = Math.min(firstMatch.getAmount(), userAmount);
+                firstMatch.setAmount(amount);
+                itemStack.setAmount(itemStack.getAmount() - amount);
+                break;
             }
         }
-        return true;
+        return firstMatch;
+    }
+
+    private void addToTarget(Player player, ItemStack stack) {
+        player.getInventory().addItem(stack).forEach(
+                // For all items which didn't fit in inventory
+                (fuckIfIKnow,remainingItemStack) -> player.getLocation().getWorld().dropItem(player.getLocation(), remainingItemStack)
+        );
     }
 
     @Override
