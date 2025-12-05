274aa9b26d859b712c142efff1c95b39d1b9358d
Aditya <youngcoder45@gmail.com>
2025-12-05 13:15:15 +0530

    feat: add Aeroburst and Expelliarmus spells, update to 1.21.10

--- src/main/java/io/github/hyscript7/ascendancy/registries/RegistryManager.java
+++ src/main/java/io/github/hyscript7/ascendancy/registries/RegistryManager.java
@@ -17,6 +17,8 @@ import io.github.hyscript7.ascendancy.features.threefold.ThreefoldIncantation;
 import io.github.youngcoder45.ascendancy.spells.Fulmen;
 import io.github.youngcoder45.ascendancy.spells.Sanatio;
 import io.github.youngcoder45.ascendancy.spells.Ventus;
+import io.github.youngcoder45.ascendancy.spells.Aeroburst;
+import io.github.youngcoder45.ascendancy.spells.Expelliarmus;
 import io.github.hyscript7.ascendancy.builtins.rituals.instant.RitualOfDeathReturn;
 import io.github.hyscript7.ascendancy.builtins.rituals.instant.RitualOfHomeComing;
 import io.github.hyscript7.ascendancy.builtins.rituals.instant.RitualOfSoulSending;
@@ -111,6 +113,8 @@ public class RegistryManager {
         spellRegistry.register(new Sanatio());
         spellRegistry.register(new Vonszol());
         spellRegistry.register(new Recall());
+        spellRegistry.register(new Aeroburst());
+        spellRegistry.register(new Expelliarmus());
         // TODO: Add spells
     }
 
