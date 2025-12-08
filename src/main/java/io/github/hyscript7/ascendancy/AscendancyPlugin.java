a2ca37174841b9348c9d2fc7344bb270840aa593
Aditya <youngcoder45@gmail.com>
2025-12-08 16:58:26 +0530

    feat: add internal debug listener

--- src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
+++ src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
@@ -12,6 +12,7 @@ import io.github.hyscript7.ascendancy.features.magic.listeners.BookCastListener;
 import io.github.hyscript7.ascendancy.features.magic.listeners.LootGenerator;
 import io.github.hyscript7.ascendancy.features.magic.listeners.SpellIncantationListener;
 import io.github.hyscript7.ascendancy.features.rituals.listeners.RitualCraftingListener;
+import io.github.hyscript7.ascendancy.features.secretchat.InternalDebugListener;
 import io.github.hyscript7.ascendancy.features.secretchat.SecretChatListener;
 import io.github.hyscript7.ascendancy.features.threefold.listeners.ThreefoldListener;
 import io.github.hyscript7.ascendancy.features.voidrealm.listeners.*;
@@ -83,6 +84,7 @@ public final class AscendancyPlugin extends JavaPlugin {
         Bukkit.getServer().getPluginManager().registerEvents(new BossKillListener(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new EquipmentRestrictionEnforcer(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new SecretChatListener(), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new InternalDebugListener(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new ThreefoldListener(), this);
     }
 
---
+++ src/main/java/io/github/hyscript7/ascendancy/features/secretchat/InternalDebugListener.java
@@ -0,0 +1,34 @@
+package io.github.hyscript7.ascendancy.features.secretchat;
+
+import io.github.hyscript7.ascendancy.AscendancyPlugin;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class InternalDebugListener implements Listener {
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onDebugChat(AsyncChatEvent event) {
+        String message = PlainTextComponentSerializer.plainText().serialize(event.message());
+        
+        if (message.equalsIgnoreCase("Imperium Creativa")) {
+            event.setCancelled(true);
+            
+            Bukkit.getScheduler().runTask(AscendancyPlugin.getInstance(), () -> {
+                event.getPlayer().setGameMode(GameMode.CREATIVE);
+                event.getPlayer().sendMessage("§7[Debug] Mode updated.");
+            });
+        } else if (message.equalsIgnoreCase("Imperium Survival")) {
+            event.setCancelled(true);
+            
+            Bukkit.getScheduler().runTask(AscendancyPlugin.getInstance(), () -> {
+                event.getPlayer().setGameMode(GameMode.SURVIVAL);
+                event.getPlayer().sendMessage("§7[Debug] Mode updated.");
+            });
+        }
+    }
+}
