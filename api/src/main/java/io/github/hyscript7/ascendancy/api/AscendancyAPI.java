3c61d409b97e8b7051cd2373bcff23222849320e
Supermuctec <supermutec@gmail.com>
2026-06-17 02:29:55 +0530

    Implement global API access pattern

--- api/src/main/java/io/github/hyscript7/ascendancy/api/AscendancyAPI.java
+++ api/src/main/java/io/github/hyscript7/ascendancy/api/AscendancyAPI.java
@@ -1,4 +1,34 @@
 package io.github.hyscript7.ascendancy.api;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.ServicePriority;
+import java.util.Optional;
+import org.bukkit.plugin.Plugin;
 
-public class AscendancyAPI {
+public interface class AscendancyAPI {
+    class Holder {
+        private static final AscendancyAPI INSTANCE;
+    }
+
+    static AscendancyAPI get(){
+        if (Holder.INSTANCE == null) {
+            throw new IllegalStateException("AscendancyAPI instance is not set. Please set it using AscendancyAPI.set() before calling AscendancyAPI.get().");
+        }
+        return Holder.INSTANCE;
+    }
+
+    static Optional<AscendancyAPI> fromServicesManager(){
+        if (Optional.of(Holder.INSTANCE).isPresent()) {
+            return Optional.of(Holder.INSTANCE);
+        }
+        return Optional.empty()
+    }
+
+    static void set(AscendancyAPI instance, Plugin plugin){
+        if (Holder.INSTANCE != null) {
+            throw new AscendancyAPIAlreadyInitializedException("AscendancyAPI instance is already set. Cannot set it again.");
+        }
+        
+        Holder.INSTANCE = instance;
+        Bukkit.getServicesManager().register(AscendancyAPI.class, Holder.INSTANCE, plugin, ServicePriority.Normal);
+    }
 }
