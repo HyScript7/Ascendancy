7d9e5dfa66ef29bf4efe0634e44b860e6d848929
Supermuctec <supermutec@gmail.com>
2026-06-18 19:13:01 +0530

    Completed ASC-5, up for review, called events on plugin startup and shutdown

--- core/src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
+++ core/src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
@@ -3,6 +3,7 @@ package io.github.hyscript7.ascendancy;
 import io.github.hyscript7.ascendancy.api.events.AscendancyDisabledEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("unused")
 public class AscendancyPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
