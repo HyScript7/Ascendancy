2787824220694894d47e8456620eb14add635107
Supermuctec <supermutec@gmail.com>
2026-06-18 22:45:30 +0530

    Add api and functionality

--- core/src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
+++ core/src/main/java/io/github/hyscript7/ascendancy/AscendancyPlugin.java
@@ -1,17 +1,25 @@
 package io.github.hyscript7.ascendancy;
 
+import io.github.hyscript7.ascendancy.api.AscendancyAPI;
 import io.github.hyscript7.ascendancy.api.events.AscendancyDisabledEvent;
+import io.github.hyscript7.ascendancy.api.events.AscendancyEnabledEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 
-@SuppressWarnings("unused")
 public class AscendancyPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        
+        // All remaining code goes above enable event, event is called when plugin is completely ready
+        AscendancyAPI api = new AscendancyAPI(){
+
+        };
+        AscendancyAPI.set(api, this);
+        Bukkit.getPluginManager().callEvent(new AscendancyEnabledEvent(api));
     }
 
     @Override
     public void onDisable() {
-        
+        Bukkit.getPluginManager().callEvent(new AscendancyDisabledEvent());
+        // All remaining code goes under disable event, event is called when plugin starts shutting down
     }
 }
